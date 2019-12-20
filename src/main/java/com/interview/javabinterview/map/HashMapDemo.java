25a2d24d4a1dafcfdc8603b619a7822dd873b007
baoquan.ju <79881065@qq.com>
2019-12-20 10:28:13 +0800

    init

---
+++ src/main/java/com/interview/javabinterview/map/HashMapDemo.java
@@ -0,0 +1,21 @@
+package com.interview.javabinterview.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 类
+ *
+ * @author Ju Baoquan
+ * Created at  2019/12/19
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        String str = "string";
+        str.hashCode();
+
+        Map<Object, Object> hashMap = new HashMap<>();
+        hashMap.put("","");
+    }
+}
